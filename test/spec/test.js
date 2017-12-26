ad93fc8df42ba3dbce2fde585c9904b1d8d42414
rohini kumR REDDY <VISHNU@rohinis-MacBook-Air.local>
2017-12-26 08:10:44 +0530

    Initial commit

---
+++ test/spec/test.js
@@ -0,0 +1,11 @@
+suite('Array', function() {
+  setup(function() {
+    // ...
+  });
+
+  suite('#should load the dom with suggeste links', function() {
+    test('No of links equal to json input', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
