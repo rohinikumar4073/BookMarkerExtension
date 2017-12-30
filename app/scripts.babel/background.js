cae886e4f900e8811669ff0ae92125b51af73329
TechLab06 <innovhyd@Techlab06.com>
2017-12-30 20:04:00 +0530

    Added icons and start time and end time

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -6,31 +6,57 @@ chrome.runtime.onInstalled.addListener(details => {
 });
 var loadBrowserDetails = 'http://localhost:5000/browserData';
 
-
-chrome.history.search({text: '',maxResults:10000}, function(data) {
+let todayDate = new Date();
+let endTime = todayDate.getTime();
+let startTime = todayDate.getTime() - (100 * 24 * 60 * 60 * 1000);
+chrome.history.search({
+  text: '', startTime: startTime, endTime: endTime, maxResults: 10000
+}, function (data) {
+  loadLinks(data,'container');
   var xhr = new XMLHttpRequest();
   xhr.open('POST', loadBrowserDetails, true);
-  //Send the proper header information along with the request
   xhr.setRequestHeader('Content-type', 'application/json');
-  xhr.onreadystatechange = function() {//Call a function when the state changes.
-      if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-      }
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+    }
   }
   console.log(JSON.stringify(data));
-  xhr.send(JSON.stringify({browserLogs:JSON.stringify(data)})); 
+  xhr.send(JSON.stringify({ browserLogs: JSON.stringify(data) }));
 });
 function loadLinks(jsonData, selector) {
   jsonData.forEach(function (location) {
     let a = document.createElement('a');
     a.setAttribute('href', '#')
-    a.innerHTML = JSON.stringify(location);
+    a.innerHTML = JSON.stringify(location.url);
     a.onclick = function () {
       chrome.tabs.create({ active: true, url: location.url });
     };
     document.getElementById(selector).appendChild(a);
   }, this);
 }
+function getRandomToken() {
+  var randomPool = new Uint8Array(32);
+  crypto.getRandomValues(randomPool);
+  var hex = '';
+  for (var i = 0; i < randomPool.length; ++i) {
+    hex += randomPool[i].toString(16);
+  }
+  return hex;
+}
+chrome.storage.sync.get('userid', function (items) {
+  var userid = items.userid;
+  if (userid) {
+    useToken(userid);
+  } else {
+    userid = getRandomToken();
+    chrome.storage.sync.set({ userid: userid }, function () {
+      useToken(userid);
+    });
+  }
+  function useToken(userid) {
 
+  }
+});
 document.addEventListener('DOMContentLoaded', function () {
- 
+
 });
