2a94a769fdafcfbbe3b048f892409ccd279b7a32
vishnu sandhireddy <vissu_nbkr@yahoo.com>
2018-01-04 10:21:13 +0530

    adding test cases

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,8 +1,36 @@
-function syncData(browserId) {
-  const loadBrowserDetails = 'http://localhost:5000/browserData';
-  let todayDate = new Date();
-  let endTime = todayDate.getTime();
-  let startTime = todayDate.getTime() - (100 * 24 * 60 * 60 * 1000);
+const loadBrowserDetails = 'http://localhost:5000/browserData';
+const syncUpDateLimit = 15;
+const fetchbrowserHistoryLimitDays = 100;
+
+const Util = new function () {
+  this.loadLinks = function (jsonData, selector) {
+    jsonData.forEach(function (location) {
+      let a = document.createElement('a');
+      a.setAttribute('href', '#')
+      a.innerHTML = JSON.stringify(location);
+      a.onclick = function () {
+        chrome.tabs.create({ active: true, url: location.url });
+      };
+      document.getElementById(selector).appendChild(a);
+    }, this);
+  };
+  this.getRandomToken = function () {
+    var randomPool = new Uint8Array(32);
+    crypto.getRandomValues(randomPool);
+    var hex = '';
+    for (var i = 0; i < randomPool.length; ++i) {
+      hex += randomPool[i].toString(16);
+    }
+    return hex;
+  };
+  this.IsCurDateExceedsSyncLimit= function (currentTime, lastSyncedDate, dateLimit) {
+    return (currentTime > (lastSyncedDate + (dateLimit * 24 * 60 * 60 * 1000)));
+  };
+};
+
+
+
+function loadBrowserHistory(startTime, endTime, browserId) {
   chrome.history.search({
     text: '', startTime: startTime, endTime: endTime, maxResults: 10000
   }, function (data) {
@@ -14,34 +42,34 @@ function syncData(browserId) {
         console.log("Data got saved successfully !")
       }
     }
-    console.log("browserId",browserId)
-
-    xhr.send(JSON.stringify({ browserLogs: JSON.stringify(data) ,browserId:browserId}));
+    xhr.send(JSON.stringify({ browserLogs: JSON.stringify(data), browserId: browserId }));
   });
 }
-function getRandomToken() {
-  var randomPool = new Uint8Array(32);
-  crypto.getRandomValues(randomPool);
-  var hex = '';
-  for (var i = 0; i < randomPool.length; ++i) {
-    hex += randomPool[i].toString(16);
+function syncData(browserId) {
+  let todayDate = new Date();
+  let endTime = todayDate.getTime();
+  let startTime = todayDate.getTime() - (fetchbrowserHistoryLimitDays * 24 * 60 * 60 * 1000);
+  loadBrowserHistory(startTime, endTime, browserId);
+}
+function setLastSyncDate(browserId, cb) {
+  chrome.storage.sync.set({ browserId: browserId, lastSyncedDate: new Date().getTime() }, cb);
+}
+function syncUpBrowserData(items) {
+  let browserId = items.browserId;
+  if (browserId) {
+    chrome.storage.sync.get('lastSyncedDate', function (items) {
+      console.log("items",items);
+
+      let today = new Date();
+      if (Util.IsCurDateExceedsSyncLimit(today.getTime, items.lastSyncedDate, syncUpDateLimit)) {
+        setLastSyncDate(browserId, function () { syncData(browserId) })
+      }
+    });
+  } else {
+    browserId = Util.getRandomToken();
+    setLastSyncDate(browserId, function () { syncData(browserId) })
   }
-  return hex;
-};
-function handleBrowserId(items) {
-    let browserId = items.browserId;
-    if (browserId) {
-      console.log('browserId id exists ', browserId);
-      syncData(browserId);
-    } else {
-      browserId = getRandomToken();
-      chrome.storage.sync.set({ browserId: browserId }, function () {
-        console.log('browserId id generated ', browserId);
-        syncData(browserId);
-      });
-    }
-  
+
 };
 console.log("running ")
-    chrome.storage.sync.get('browserId', function (items) { handleBrowserId(items); });
-  
+chrome.storage.sync.get('browserId', function (items) { syncUpBrowserData(items); });
--- test/spec/test.js
+++ test/spec/test.js
@@ -5,8 +5,13 @@ suite('Bookmarker Links', function () {
   suite('#should load the dom with suggeste links', function () {
     test('No of links equal to json input', function () {
       let jsonData = ["https://www.google.com", "https://www.cricbuzz.com"];
-      loadLinks(jsonData, "mocha");
+      Util.loadLinks(jsonData, "mocha");
       assert(document.getElementById("mocha").childElementCount, 2)
     });
+    test('Check for Date exceeded', function () {
+      assert.equal(true, Util.IsCurDateExceedsSyncLimit(1514766670000,1483230670000,30));
+      assert.equal(false, Util.IsCurDateExceedsSyncLimit(1514766670000,1513729870000,30));
+    });
+   
   });
 });
